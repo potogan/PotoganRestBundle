62add3d2092d073bd4b2bc998aa5d5343a4e1d10
Pierre Dudoret <pierre.dudoret@potogan.fr>
2018-01-24 18:23:24 +0100

    Updated dependencies and injections according to latest potogan/rest updates

--- src/PotoganRESTBundle.php
+++ src/PotoganRESTBundle.php
@@ -4,17 +4,7 @@ namespace Potogan\RESTBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Potogan\RESTBundle\DependencyInjection\Compiler\GenericChainService;
 
 class PotoganRESTBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new GenericChainService());
-    }
 }
